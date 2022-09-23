869da5a7b6ab1a7b3eec663203522c4825cece33
Per Norin <pelle.norin.79@gmail.com>
2022-09-23 23:07:46 +0200

    Basic file structure. Contact model created.

---
+++ Services/MenuService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook.Services
+{
+    internal class MenuService
+    {
+
+        // main menu
+        // create contact menu
+        // view all menu
+        // view one menu -> delete/edit - menu
+    }
+}
